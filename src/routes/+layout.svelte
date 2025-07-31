ec58e500e3131c3e84ad047353fcfa6783c33c93
Ronjar <dev@robingebert.com>
2025-07-31 19:55:58 +0200

    Added a logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import "../app.css";
+	import logo from "../yact.png";
 	import { ModeWatcher } from "mode-watcher";
 	import ThemeToggle from "$lib/components/custom/ThemeToggle.svelte";
 	import { Toaster } from "$lib/components/shadcn/sonner/index.js";
 	import { sessionMeta } from "$lib/stores/session";
 	import Button from "$lib/components/shadcn/button/button.svelte";
-    import ClipboardIcon from "@lucide/svelte/icons/clipboard-list";
-    import { toast } from "svelte-sonner";
+	import ClipboardIcon from "@lucide/svelte/icons/clipboard-list";
+	import { toast } from "svelte-sonner";
 
 	let code: string | undefined = $state();
 	sessionMeta.subscribe(({ code: c }) => (code = c));
@@ -27,10 +28,20 @@
 <div
 	class="flex h-18 w-full justify-between gap-2 items-center px-4 shadow-md fixed top-0"
 >
-	<h1 class="text-3xl font-bold">Yact</h1>
+	<Button variant="ghost" class="flex flex-row items-center gap-2 cursor-pointer rounded-xl py-6">
+		<img alt="YACT logo" class="size-8 bg-white rounded-lg p-1" src={logo} />
+		<h1 class="text-2xl font-bold">Yact</h1>
+	</Button>
 
 	{#if code}
-		<Button onclick={copy} variant="ghost" class="group cursor-pointer flex flex-row items-center gap-2 font-mono text-2xl p-6">{code}<ClipboardIcon class="size-6 pb-1 opacity-0 transition-opacity group-hover:opacity-75 cursor-pointer"/></Button>
+		<Button
+			onclick={copy}
+			variant="ghost"
+			class="group cursor-pointer flex flex-row items-center gap-2 font-mono text-2xl p-6"
+			>{code}<ClipboardIcon
+				class="size-6 pb-1 opacity-0 transition-opacity group-hover:opacity-75 cursor-pointer"
+			/></Button
+		>
 	{/if}
 
 	<ThemeToggle />
